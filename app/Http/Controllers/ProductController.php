1629137c4179aebe1475788dc516e1a2d1f0e27f
Moizac <izfa87aremania@gmail.com>
2020-10-14 15:34:01 +0700

    change format response for index and show

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,13 +9,19 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        return $products;
+        return response()->json([
+            'message' => 'Menampilkan Semua Produk',
+            'data' => $products
+        ], 200);
     }
     public function show($id)
     {
         $products = Product::find($id);
         if($products){
-            return $products;
+            return response()->json([
+                'message' => 'Produk Berhasil Ditemukan',
+                'data' => $products
+            ], 200);
         }else{
             return response()->json([
                 'message' => 'Produk Tidak Ada'
